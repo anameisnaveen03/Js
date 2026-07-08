ef5d53c6b9924c9cd3e495016e3eea8599d32a3a
anameisnaveen03 <nsoni8102@gmail.com>
2026-07-08 22:27:25 +0530

    day4

---
+++ Strings/string.js
@@ -0,0 +1,11 @@
+let num = 10;
+let str = "Hello World";
+let str1 = new String("  Hello  ");
+let str2 = str + str1;
+console.log(str.length());
+console.log(str1.trim());
+console.log(str+str1);
+console.log(str.substring(1,5));
+
+
+
